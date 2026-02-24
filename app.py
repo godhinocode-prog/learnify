2a9d50abe029b850c8899c4b332696373820bb01
godhinocode-prog <godhinocode@gmail.com>
2026-02-24 16:15:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -378,4 +378,5 @@ def get_stats():
 
 if __name__ == "__main__":
     print("🚀 Learnify server starting on http://localhost:5000")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
